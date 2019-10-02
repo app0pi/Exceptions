e13e6552946e675cdb4b2f4390ae0b9cdef74e5d
Kenny Lieu <kennylieu@mines.edu>
2019-10-02 11:55:02 -0600

    created ATM instance variable

---
+++ src/ATM.java
@@ -0,0 +1,5 @@
+
+public class ATM {
+	private BankAccount account;
+	
+}
---
+++ src/NegativeBalanceException.java
@@ -0,0 +1,12 @@
+
+public class NegativeBalanceException extends Exception {
+	private double negativeBalance;
+	//private String message = 
+
+	public NegativeBalanceException() {
+		//String message = "ham";
+		super();
+		
+	}
+	
+}
